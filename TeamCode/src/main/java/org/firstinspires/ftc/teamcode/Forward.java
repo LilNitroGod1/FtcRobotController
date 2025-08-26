0de5113a2d49ed472fdf5454707aaa73a5db1645
Gahan Mummidi <gahanmummidi@gmail.com>
2025-08-26 13:02:38 -0700

    Included pinpoint initialization encoder motors and servo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Forward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Forward.java
@@ -1,16 +1,54 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "telemetry test")
+@TeleOp(name = "FTC Coding Camp")
 public class Forward extends LinearOpMode {
 
+    private DcMotor right_front, right_back, left_front, left_back;
+    private DcMotor slider_extend, slider_pivot;
+    private Servo claw_open, claw_rotate, claw_pivot;
+    private DistanceSensor distance_sensor;
+
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
+        right_front = hardwareMap.dcMotor.get("right_front");
+        right_back = hardwareMap.dcMotor.get("right_back");
+        left_front = hardwareMap.dcMotor.get("left_front");
+        left_back = hardwareMap.dcMotor.get("right_front");
+
+        claw_open = hardwareMap.servo.get("claw_open");
+
+        left_back.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Robot status", "Initialized");
+        telemetry.update();
 
         waitForStart();
 
-        telemetry.addLine("DS did not crash.");
+        moveMotorToPos(1000);
+
+        if (gamepad1.a) {
+            claw_open.setPosition(1);
+        } else if (gamepad1.b) {
+            claw_open.setPosition(0.5);
+        } else {
+            claw_open.setPosition(0);
+        }
+    }
+
+    public void moveMotorToPos(int encoderPos) {
+        right_front.setPower(0.5);
+        while (right_front.getCurrentPosition()  < encoderPos) {
+            telemetry.addData("Motor Position: ", right_front.getCurrentPosition());
+            telemetry.update();
+        }
     }
 }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinPoint.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+public class PinPoint extends LinearOpMode {
+
+    private GoBildaPinpointDriver pinpoint;
+
+    @Override
+    public void runOpMode() {
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+
+        pinpoint.setOffsets(66.0, 42.0, DistanceUnit.MM);
+        pinpoint.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+        pinpoint.resetPosAndIMU();
+        pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0));
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            pinpoint.update();
+
+            Pose2D pose2D = pinpoint.getPosition();
+
+            telemetry.addData("X coordinate (IN)", pose2D.getX(DistanceUnit.INCH));
+            telemetry.addData("Y coordinate (IN)", pose2D.getY(DistanceUnit.INCH));
+            telemetry.addData("Heading angle (DEGREES)", pose2D.getHeading(AngleUnit.DEGREES));
+            telemetry.update();
+        }
+    }
+}
