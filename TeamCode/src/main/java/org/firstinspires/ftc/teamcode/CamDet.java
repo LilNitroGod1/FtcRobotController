917d04fcea18ecc1cc13df47542b6ced28dd5f8c
Gahan Mummidi <gahanmummidi@gmail.com>
2025-09-07 08:07:33 -0700

    fixed & to % in telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CamDet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CamDet.java
@@ -42,13 +42,13 @@ public class CamDet extends LinearOpMode {
         while (opModeIsActive()) {
             for (AprilTagDetection detection : aprilTagProcessor.getDetections()) {
                 if (detection.metadata != null) {
-                    telemetry.addLine(String.format("\n==== (ID &d) &s", detection.id, detection.metadata.name));
-                    telemetry.addLine(String.format("XYZ &6.1f &6.1f &6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                    telemetry.addLine(String.format("PRY &6.1f &6.1f &6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                    telemetry.addLine(String.format("RBE &6.1f &6.1f &6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                    telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                    telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
                 } else {
-                    telemetry.addLine(String.format("\n==== (ID &d) Unknown", detection.id));
-                    telemetry.addLine(String.format("Center &6.0f &6.0f   (pixels)", detection.center.x, detection.center.y));
+                    telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                    telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
                 }
             }  //end for() loop
 
